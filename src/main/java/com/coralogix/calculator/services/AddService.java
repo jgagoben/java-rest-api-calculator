bc008d0f4509f093060f55bb41c63e1ace44019c
jgagben <jgagben828@g.educaand.es>
2022-10-05 15:35:36 +0200

    Error induced

--- src/main/java/com/coralogix/calculator/services/AddService.java
+++ src/main/java/com/coralogix/calculator/services/AddService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class AddService {
     public Result add(int a, int b) {
-        int sum = a + b;
+        int sum = a + b + 1;
         return new Result(sum);
     }
 }
